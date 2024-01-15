92ab3b184206613321a018c535e1c66fb61a3fb1
RafaTeron <rafael_sabreu@hotmail.com>
2024-01-15 10:42:18 -0300

    Criando Projeto

---
+++ src/main/java/com/rafaelAbreu/JogoQuiz/JogoQuizApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelAbreu.JogoQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelAbreu/JogoQuiz/JogoQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelAbreu.JogoQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
