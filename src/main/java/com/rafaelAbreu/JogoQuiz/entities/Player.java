3e3a5bb2d75b78c2b0d5d2b8095a4c7a23ddea93
RafaTeron Abreu <rafael_sabreu@hotmail.com>
2024-01-17 15:56:17 +0000

    Model Player

---
+++ src/main/java/com/rafaelAbreu/JogoQuiz/entities/Player.java
@@ -0,0 +1,42 @@
+package com.rafaelAbreu.JogoQuiz.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Player implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Player(){
+        
+    }
+ 
+    public Player(Long id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
